e25a8df2e7293b5731dcd98a7e8caec53f2a693c
Raja-Afzaal <afzalraja0307@gmail.com>
2025-03-23 20:22:49 +0500

    Open-Ended.cpp

--- Open-Ended.cpp
+++ Open-Ended.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-#define MAX_ITEMS 10 
+ const int MAX_ITEMS = 10; 
 
 struct MenuItem 
 {
@@ -23,15 +23,15 @@ struct Order
 };
 
 MenuItem breakfastMenu[MAX_ITEMS] = {
-    {1, "1.Baghrir", 500, 10}, {2, "2. Msemen", 400, 8}, {3, "3.Harira Soup", 700, 15}
+    {1, "1.Baghrir", 600, 10}, {2, "2. Msemen", 500, 10}, {3, "3.Harira Soup", 300, 10}
 };
 
 MenuItem lunchMenu[MAX_ITEMS] = {
-    {1, "1.Chicken Pastilla", 1000, 10}, {2, "2.Maakouda ", 1100, 5}, {3, "3.Zaalouk", 900, 20}
+    {1, "1.Chicken Pastilla", 800, 10}, {2, "2.Maakouda ", 1000, 10}, {3, "3.Zaalouk", 700, 10}
 };
 
 MenuItem dinnerMenu[MAX_ITEMS] = {
-    {1, "1.Kefta Tagine", 1500, 7}, {2, "2.Mechoui", 1200, 10}, {3, "3.Fish Chermoula", 1500, 12}
+    {1, "1.Kefta Tagine", 1500, 10}, {2, "2.Mechoui", 1200, 10}, {3, "3.Fish Chermoula", 700, 10}
 };
 
 void getMenu(MenuItem menu[], int &size) 
@@ -41,21 +41,21 @@ void getMenu(MenuItem menu[], int &size)
     int hour = ltm->tm_hour;
 
     if (hour >= 6 && hour < 11) 
-	{
+    {
         cout << "Breakfast Menu:\n";
         size = 3;
         for (int i = 0; i < size; i++)
             menu[i] = breakfastMenu[i];
     } 
-	else if (hour >= 11 && hour < 16) 
-	{
+    else if (hour >= 11 && hour < 16) 
+    {
         cout << "Lunch Menu:\n";
         size = 3;
         for (int i = 0; i < size; i++)
             menu[i] = lunchMenu[i];
     } 
-	else 
-	{
+    else 
+    {
         cout << "Dinner Menu:\n";
         size = 3;
         for (int i = 0; i < size; i++)
@@ -68,68 +68,90 @@ void takeOrder(Order &order, MenuItem menu[], int size)
     int choice, qty;
     order.itemCount = 0;
 
-    cout << "\nEnter item number to order (0 to finish): ";
+    cout << "\nEnter item number to order (0 to stop): ";
     while (true) 
-	{
+    {
         cout << "\n Item No: ";
         cin >> choice;
         if (choice == 0) 
-		break;
+        break;
         if (choice > 0 && choice <= size) 
-		{
+        {
             cout << "\n Quantity: ";
             cin >> qty;
 
             if (menu[choice - 1].stock >= qty) 
-			{
+            {
                 order.itemId[order.itemCount] = choice;
                 order.quantity[order.itemCount] = qty;
                 menu[choice - 1].stock -= qty;
                 order.itemCount++;
             } 
-			else 
-			{
+            else 
+            {
                 cout << "Sorry, We are out of stock!\n";
             }
         } 
-		else 
-		{
+        else 
+        {
             cout << "Invalid choice, try again.\n";
         }
     }
 }
 
 void generateBill(Order &order, MenuItem menu[], int size) {
-    double total = 0;
-    cout << "\n----- \tTotal Bill\t -----\n";
-    cout << setw(10) << "Item" << setw(10) << "Qty" << setw(10) << "Price" << setw(10) << "Total" << endl;
-    cout << "--------------------------------------\n";
+    double total = 0, discount = 0, grandTotal = 0;
+    cout << setfill('=') << setw(50) << "" << endl;
+    cout << setw(30) << setfill(' ') << " RECEIPT " << setw(20) << endl;
+    cout << setfill('=') << setw(50) << "" << endl;
+    
+    cout << left << setfill(' ') 
+         << setw(20) << "Item" 
+         << setw(10) << "Qty" 
+         << setw(10) << "Price" 
+         << setw(10) << "Total" << endl;
+    
+    cout << setfill('-') << setw(50) << "" << endl;
 
-    for (int i = 0; i < order.itemCount; i++) 
-	{
+    for (int i = 0; i < order.itemCount; i++) {
         int id = order.itemId[i] - 1;
         double price = menu[id].price;
         double itemTotal = price * order.quantity[i];
         total += itemTotal;
 
-        cout << setw(10) << menu[id].name << setw(10) << order.quantity[i] << setw(10) << price << setw(10) << itemTotal << endl;
+        cout << left << setfill(' ') 
+             << setw(20) << menu[id].name 
+             << setw(10) << order.quantity[i] 
+             << setw(10) << price 
+             << setw(10) << itemTotal << endl;
     }
-    cout << "--------------------------------------\n";
-    cout << "Grand Total: RS." << total << endl;
+
+    if (total > 5000) {
+        discount = total * 0.10;
+    }
+    grandTotal = total - discount;
+
+    cout << setfill('-') << setw(50) << "" << endl;
+    cout << setfill(' ') << left 
+         << setw(30) << "Discount Applied: " 
+         << "RS." << discount << " (10%)" << endl;
+    cout << left << setw(30) << "Grand Total: " 
+         << "RS." << grandTotal << endl;
+    cout << setfill('=') << setw(50) << "" << endl;
 }
 
 void saveOrderToFile(Order &order, MenuItem menu[], int size) {
     ofstream file("orders.txt", ios::app);
     if (!file) 
-	{
+    {
         cout << "Error opening file!\n";
         return;
     }
     file << "--- Order ---\n";
     for (int i = 0; i < order.itemCount; i++) 
-	{
+    {
         int id = order.itemId[i] - 1;
-        file << menu[id].name << " x " << order.quantity[i] << " = $" << menu[id].price * order.quantity[i] << "\n";
+        file << menu[id].name << " x " << order.quantity[i] << " = RS." << menu[id].price * order.quantity[i] << "\n";
     }
     file << "-------------------\n";
     file.close();
@@ -148,15 +170,25 @@ void saveInventory(MenuItem menu[], int size) {
 }
 
 void displayInventory(MenuItem menu[], int size) {
-    cout << "\n--- Inventory Status ---\n";
-    cout << setw(10) << "Item" << setw(10) << "Stock" << endl;
-    cout << "--------------------------\n";
+    cout << setfill('=') << setw(40) << "" << endl;
+    cout << setw(20) << setfill(' ') << " INVENTORY " << setw(20) << endl;
+    cout << setfill('=') << setw(40) << "" << endl;
+    cout << left << setfill(' ') << setw(5) << "No." 
+         << setw(25) << "Item Name" 
+         << setw(10) << "Stock" << endl;
+    cout << setfill('-') << setw(40) << "" << endl;
+
     for (int i = 0; i < size; i++) {
-        cout << setw(10) << menu[i].name << setw(10) << menu[i].stock << endl;
+        cout << left << setfill(' ') << setw(5) << menu[i].id 
+             << setw(25) << menu[i].name 
+             << setw(10) << menu[i].stock << endl;
     }
+    cout << setfill('=') << setw(40) << "" << endl;
 }
 
+
 int main() {
+    cout << " ====WELCOME==== " << endl;
     MenuItem menu[MAX_ITEMS];
     int size;
     Order order;
@@ -171,4 +203,3 @@ int main() {
     cout << "\nThank you for visiting us!\n";
     return 0;
 }
-
